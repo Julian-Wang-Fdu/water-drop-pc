610420e4d0eb386e0b1e09a033feb36ff6a3e523
RoxanneLuo <roxanneluo918@gmail.com>
2024-10-07 15:58:21 -0700

    init integration test

---
+++ src/graphql/auth.ts
@@ -0,0 +1,11 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN = gql`
+mutation login($account: String!,$password:String!){
+  login(account:$account,
+    password:$password
+){
+    code,
+    message
+  }}
+`;
